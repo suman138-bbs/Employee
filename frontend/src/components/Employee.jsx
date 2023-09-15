25534eb516b1c44abe8ea39732480bb570bfe958
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-09-15 12:13:33 +0530

    profile component completed

--- frontend/src/components/Employee.jsx
+++ frontend/src/components/Employee.jsx
@@ -1,39 +1,57 @@
-import '../index.css'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 const Employee = () => {
   const [data, setData] = useState([]);
-  const [message,setMessage] = useState('')
-        useEffect(() => {
-            try {
-                const getData = async() => {
-                const res = await axios.get('http://localhost:8080/getEmployees');
-               
-                setData(res.data.Result)
-                
-            }
-          getData()
-            } catch (error) {
-                console.log(error)
-            }
-        }, [message])
-    const handleDelete = async(id) => {
+  const [message, setMessage] = useState('');
+  const [searchQuery, setSearchQuery] = useState(''); // State for the search query
+
+  useEffect(() => {
+    const getData = async () => {
       try {
-        const res = await axios.delete('http://localhost:8080/deleteEmployee/'+ id)
-        console.log("From Backend", res)
-        setMessage(res)
+        const res = await axios.get('http://localhost:8080/getEmployees');
+        setData(res.data.Result);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
+    };
+    getData();
+  }, [message]);
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await axios.delete('http://localhost:8080/deleteEmployee/' + id);
+      console.log("From Backend", res);
+      setMessage(res);
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  // Filter employees based on the search query
+  const filteredData = data.filter((employee) =>
+    employee.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className='px-5 py-3 '>
       <div className='d-flex justify-content-center mt-2'>
         <h3>Employee List</h3>
       </div>
-      <Link to="/create" className='btn btn-success'>Add Employee</Link>
+      
+      <div style={{ display: 'flex', gap:'41em'}}>
+        <Link to='/create' className='btn btn-success'>Add Employee</Link>
+        <input
+          style={{borderRadius:"0.5em", backgroundColor:"#082a6e" ,border:'none',padding:'0.5em'}}
+          type='text'
+          placeholder='Search by name'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)} // Update searchQuery state when input changes
+        />
+      </div>
       <div className='mt-3 bg-white employee-list-container'>
+        
         <table className='table '>
           <thead>
             <tr>
@@ -47,27 +65,37 @@ const Employee = () => {
             </tr>
           </thead>
           <tbody className=''>
-            {data.map(({email,address,salary, name, id,image,department }) => {
-              return <tr key={id}>
-                 <Link to={`/profile/${id}`}> <td>{name}</td></Link>
-                  <td>{
-                    <img src={`http://localhost:8080/images/`+image} alt="" className='employee_image'/>
-                    }</td>
+            {filteredData.map(({ email, address, salary, name, id, image, department }) => {
+              return (
+                <tr key={id}>
+                  <Link to={`/profile/${id}`}>
+                    <td>{name}</td>
+                  </Link>
+                  <td>
+                    {
+                      <img src={`http://localhost:8080/images/` + image} alt="" className='employee_image' />
+                    }
+                  </td>
                   <td>{email}</td>
                   <td>{address}</td>
                   <td>{salary}</td>
                   <td>{department}</td>
                   <td>
-                    <Link to={`/employeeEdit/`+id} className='btn btn-primary btn-sm me-2'>edit</Link>
-                    <button onClick={e => handleDelete(id)} className='btn btn-sm btn-danger'>delete</button>
+                    <Link to={`/employeeEdit/` + id} className='btn btn-primary btn-sm me-2'>
+                      edit
+                    </Link>
+                    <button onClick={() => handleDelete(id)} className='btn btn-sm btn-danger'>
+                      delete
+                    </button>
                   </td>
-              </tr>
+                </tr>
+              );
             })}
           </tbody>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Employee
+export default Employee;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,33 +1,86 @@
 
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import axios from "axios"
-const Profile = () => {
-  const { eid } = useParams()
-  const [detail, setDetail] = useState('')
-  useEffect(()=> {
-  async  function  getData () {
-        const data = await  axios.get('http://localhost:8080/get/'+eid)
-       setDetail(data.data.Result[0])
-    }
-    getData()
-  }, [])
-  console.log(detail)
-  const {id,image} = detail
-  return (
-    <div>
-      <h1><img src={`http://localhost:8080/images/`+image} alt="" className='empImg'/></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-      <h1></h1>
-    </div>
-  )
-}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import '../index.css'
 
-export default Profile
+const Profile = () => {
+  const { eid } = useParams();
+  const [detail, setDetail] = useState('');
+
+  useEffect(() => {
+    async function getData() {
+      const data = await axios.get('http://localhost:8080/get/' + eid);
+      setDetail(data.data.Result[0]);
+    }
+    getData();
+  }, []);
+
+  console.log(detail);
+
+  const { image, name, email,address,contact ,age,education,gender,department,post,salary,training} = detail;
+
+  return (
+    <div className=" profile-container" >
+      <div className="d-flex justify-content-center"><img src={`http://localhost:8080/images/` + image} alt="" className='  empImg'/></div>
+    <div className="mt-2">
+      <table style={{ borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black", padding:'0.5em' }}>Name</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Email</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Age</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Gender</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Address</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Contact</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Education</th>
+            
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid black", padding:'0.5em'  }}>{name}</td>
+            <td style={{ border: "1px solid black", padding:'0.5em'  }}>{email}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{age}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{gender}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{address}</td>
+            <td style={{ border: "1px solid black", padding:'0.5em'  }}>{contact}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{education}</td>
+            
+          </tr>
+        </tbody>
+        </table>
+        
+      </div>
+      <div className="mt-2">
+      <table style={{ borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black", padding:'0.5em' }}>Department</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Post</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Salary</th>
+            <th style={{ border: "1px solid black", padding:'0.5em'  }}>Training</th>
+           
+            
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid black", padding:'0.5em'  }}>{department}</td>
+            <td style={{ border: "1px solid black", padding:'0.5em'  }}>{post}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{salary}</td>
+            <td style={{ border: "1px solid black" , padding:'0.5em' }}>{training}</td>
+           
+            
+          </tr>
+        </tbody>
+        </table>
+        
+    </div>
+    </div>
+  );
+};
+
+export default Profile;
